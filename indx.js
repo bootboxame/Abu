a74daed9a882205827e8ab9e14854eae64bd4564
bootboxame <79150720+bootboxame@users.noreply.github.com>
2024-04-22 03:02:41 -0700

    Update indx.js

--- indx.js
+++ indx.js
@@ -19,7 +19,7 @@ form.addEventListener("submit",(e)=>{
 //  function for checking username and password
 
 function authentication(username,password){
-	if(username === "1010597$" && password === "Daddy@170"){
+	if(username === "Char59$" && password === "Positive@7"){
 		return true
 	}else{
 		return false
